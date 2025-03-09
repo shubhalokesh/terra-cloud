8ee7a9a3c62d9f0f0444551ae82b5191d22739a7
Harisha G <hhgsharish@gmail.com>
2025-03-09 18:22:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ module "compute" {
   source        = "./ec2"
 }
 
+
 output "ec2_public_ip" {
-  value = aws_instance.ourfirst.public_ip
-  description = "Public IP of the EC2 instance"
+  value = module.compute.ec2_public_ip
+  description = "Public IP of the EC2 instance from the EC2 module"
 }
