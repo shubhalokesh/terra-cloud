5feca7b93132837b6b5346e8010c5eac689c548a
Harisha G <hhgsharish@gmail.com>
2025-03-09 18:07:07 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -28,16 +28,15 @@ resource "aws_security_group" "webserver_access" {
 }
 
 resource "aws_instance" "ourfirst" {
-  ami           = "ami-0cb91c7de36eed2cb"
-  availability_zone = "us-east-2a"
-  instance_type = "t2.micro"
-  user_data = filebase64("install_ansible.sh")
-  security_groups        = ["${aws_security_group.webserver_access.name}"]
+  ami                    = "ami-0cb91c7de36eed2cb"
+  availability_zone      = "us-east-2a"
+  instance_type          = "t2.micro"
+  user_data              = filebase64("install_ansible.sh")
+  vpc_security_group_ids = [aws_security_group.webserver_access.id]  # Correct reference
   key_name               = "revision"
+  
   tags = {
-    Name  = "ec2-test"
-    Location = "Mumbai"
+    Name      = "ec2-test"
+    Location  = "Mumbai"
   }
-
-
 }
